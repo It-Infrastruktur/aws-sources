e498b0bcb43ec6bd0ce224b5b38c76c9e59f6192
Felix Anhalt <felix.anhalt@camunda.com>
2019-11-15 13:35:00 +0100

    make category optional

--- js/index.js
+++ js/index.js
@@ -3,10 +3,15 @@ const newsapi = new NewsAPI('8566b11f65a14d54b8be47b1c01db39e');
 
 exports.handler = async (event) => {
 
-  return newsapi.v2.sources({
-    category: `${event["queryStringParameters"]['category']}`,
+  const params = {
     language: `${event["queryStringParameters"]['language']}`
-  }).then((resp) => {
+  };
+
+  if(`${event["queryStringParameters"]['category']}` !== 'undefined') {
+    params.category = `${event["queryStringParameters"]['category']}`;
+  }
+
+  return newsapi.v2.sources(params).then((resp) => {
     const response = {"isBase64Encoded": false,
                     "statusCode": 200,
                     "headers": { "headerName": "headerValue" }
